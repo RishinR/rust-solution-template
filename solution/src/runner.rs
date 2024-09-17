d8ff3a73e34809713555bf40bcf4bf8bd285597a
RishinR <rishinrnew2020@gmail.com>
2024-09-17 23:05:15 +0530

    initial commit pushing local repo to remote

---
+++ solution/src/runner.rs
@@ -0,0 +1,36 @@
+use crate::*;
+
+pub fn run_line(line: &str, tl: &mut TodoList) {
+    if let Ok((_, q)) = parser::query(line) {
+        match run_query(q, tl) {
+            Ok(r) => {
+                println!("{}", r);
+            }
+            Err(e) => {
+                eprintln!("Error: {}", e);
+            }
+        }
+    }
+}
+
+fn run_query(q: Query, tl: &mut TodoList) -> Result<QueryResult, QueryError> {
+    match q {
+        Query::Add(desc, tags) => {
+            // unimplemented!()
+            let listitem = tl.push(desc, tags);
+            return Ok(QueryResult::Added(listitem));
+        }
+        Query::Done(idx) => {
+            tl.done_with_index(idx);
+            return Ok(QueryResult::Done);
+        }
+        Query::Search(params) => {
+            let mut output = Vec::new();
+            let result = tl.search(params);
+            for i in result{
+                output.push(i.clone());
+            }
+            return Ok(QueryResult::Found(output));
+        }
+    }
+}
